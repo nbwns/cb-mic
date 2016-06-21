985e82d23c2c8c161714a1f5a162dd1332baed0d
Nicolas Bauwens <nicolas.bauwens@gmail.com>
2016-06-21 16:09:43 +0200

    merged luis bot integration

--- main.js
+++ main.js
@@ -2,21 +2,16 @@ var restify = require('restify');
 var builder = require('botbuilder');
 var drivers = require('./drivers.json').Availabilities;
 
-// Create bot and add dialogs
-var bot = new builder.BotConnectorBot({ appId: 'CBMIC', appSecret: '7f1e0a1a1bf84c49b88b56f35c146a2e' });
-bot.add('/', function (session) {
+var _ = require('underscore');
+var chrono = require('chrono-node');
+var moment = require("moment");
 
-	console.log(session.message);
+// Create LUIS Dialog that points at our model and add it as the root '/' dialog for our Cortana Bot.
+var model = 'https://api.projectoxford.ai/luis/v1/application?id=55a4b1cc-68ae-4f7c-a24f-48bedcad9950&subscription-key=52ee0f06b6484151a597ac50909ea697';
+var dialog = new builder.LuisDialog(model);
+var bot = new builder.BotConnectorBot({ appId: 'CBMIC', appSecret: '7f1e0a1a1bf84c49b88b56f35c146a2e' });//.TextBot();
+bot.add('/', dialog);
 
-    session.send(session.message.text);
-});
-
-// Setup Restify Server
-var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
-});
 
 function findDriversByDay(dayToCheck){
 	dayToCheck = dayToCheck.toLowerCase();
@@ -71,3 +66,37 @@ var driversByDay = sortDriversByDay();
 // console.log('DRIVERS FOR WEDNESDAY:', findDriversByDay('Wednesday'));
 
 console.log(findDriversPerHour(findDriversByDay('wednesday'), '1100'));
+
+
+
+// Add intent handlers
+dialog.on('needdriver', function (session, args, next) {
+    console.log(session);    
+    // Resolve and store any entities passed from LUIS.
+    console.log(args.entities);
+    var date = builder.EntityRecognizer.findEntity(args.entities, 'day');
+    var start = builder.EntityRecognizer.findAllEntities(args.entities, 'starthour');
+    var compiledStart = _.map(start,function(item){ return item.entity}).join(' ');
+    console.log("start", date.entity + " " + compiledStart);
+    var end = builder.EntityRecognizer.findAllEntities(args.entities, 'endhour');
+    var compiledEnd = _.map(end,function(item){ return item.entity}).join(' ');
+    console.log("end", date.entity + " " + compiledEnd);
+    var startDate = chrono.parseDate(date.entity + " at " + compiledStart);
+    var endDate = chrono.parseDate(date.entity + " at " + compiledEnd);
+    
+    
+    
+    
+    /*console.log("date",date);
+    console.log("start",start);
+    console.log("end",end);*/
+    console.log("startDate",startDate);
+    console.log("endDate",endDate);
+    
+    var cleanedStartDate=moment(startDate);
+    var cleanedEndDate=moment(endDate);
+    var drivers = findDriversPerHour(findDriversByDay(date.entity), cleanedStartDate.format("HHmm"))
+    console.log(drivers);
+    session.send("ok on " + date.entity + " from " + cleanedStartDate.format("HHmm") + " to " + cleanedEndDate.format("HHmm")) ;
+    });
+
